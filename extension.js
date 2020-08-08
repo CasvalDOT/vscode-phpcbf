3b4088618bc930e7a6d62383445d4341be8098d6
Casval <fabrizio.gortani@gmail.com>
2020-08-08 13:12:19 +0200

    Clear code

--- extension.js
+++ extension.js
@@ -1,314 +1,90 @@
 'use strict'
+
 const vscode = require('vscode')
 const { commands, workspace, window, languages, Range, Position } = vscode
-const path = require('path')
-const fs = require('fs')
-const os = require('os')
-const cp = require('child_process')
-const TmpDir = os.tmpdir()
+const PHPCBF = require('./Phpcbf')
+const VSCODE_COMMAND = 'phpcbf-soderlind'
 
-class PHPCBF {
-  constructor () {
-    this.loadSettings()
-  }
+// TODO
+// Check if break all
+const phpcbf = new PHPCBF()
 
-  loadSettings () {
-    const config = workspace.getConfiguration(
-      'phpcbf',
-      window.activeTextEditor.document.uri
-    )
-    if (!config.get('enable') === true) {
-      return
-    }
-    this.onsave = config.get('onsave', false)
+/**
+ * Check if current document is a php file
+ *
+ * @param {object} payload the event provided
+ * @returns {boolean}
+ * */
+function isPhp (payload) {
+  return payload.document.languageId === 'php'
+}
 
-    this.executablePath = config.get(
-      'executablePath',
-      process.platform === 'win32' ? 'php-cbf.bat' : 'phpcbf'
-    )
+/**
+ * Check if format on save is enables
+ * in the vscode settings
+ *
+ * @returns {boolean}
+ * */
+function isFormatOnSaveEnabled () {
+  const editor = window.activeTextEditor
+  return (
+    workspace
+      .getConfiguration('editor', editor.document.uri)
+      .get('formatOnSave') === true
+  )
+}
 
-    this.configSearch = config.get('configSearch', false)
-
-    /**
-     * relative paths?
-     */
-
-    // ${workspaceRoot} is deprecated
-    if (this.executablePath.startsWith('${workspaceRoot}')) {
-      this.addRootPath('${workspaceRoot}')
-    }
-    if (this.executablePath.startsWith('${workspaceFolder}')) {
-      this.addRootPath('${workspaceFolder}')
-    }
-    if (this.executablePath.startsWith('.')) {
-      this.addRootPath('.')
-    }
-    if (this.executablePath.startsWith('~')) {
-      this.executablePath = this.executablePath.replace(
-        /^~\//,
-        os.homedir() + '/'
-      )
-    }
-
-    this.standard = config.get('standard', null)
-
-    this.documentFormattingProvider = config.get(
-      'documentFormattingProvider',
-      true
-    )
-
-    this.debug = config.get('debug', false)
-  }
-
-  getArgs (document, tmpFileName) {
-    const args = []
-    if (this.debug) {
-      args.push('-l')
-    } else {
-      args.push('-lq')
-    }
-    args.push(tmpFileName)
-
-    this.standard = this.getStandard(document)
-
-    if (this.standard) {
-      args.push('--standard=' + this.standard)
-    }
-    if (this.debug) {
-      console.group('PHPCBF')
-      console.log('PHPCBF args: ' + this.executablePath + ' ' + args.join(' '))
-    }
-    return args
-  }
-
-  getStandard (document) {
-    // Check if a config file exists and handle it
-    let standard = null
-    const folder = workspace.getWorkspaceFolder(document.uri)
-    const workspaceRoot = folder ? folder.uri.fsPath : null
-    const filePath = document.fileName
-    if (this.configSearch && workspaceRoot !== null && filePath !== undefined) {
-      const confFileNames = [
-        '.phpcs.xml',
-        '.phpcs.xml.dist',
-        'phpcs.xml',
-        'phpcs.xml.dist',
-        'phpcs.ruleset.xml',
-        'ruleset.xml'
-      ]
-
-      const fileDir = path.relative(workspaceRoot, path.dirname(filePath))
-      const confFile = this.findFiles(workspaceRoot, fileDir, confFileNames)
-
-      standard = confFile || this.standard
-    } else {
-      standard = this.standard
-    }
-
-    return standard
-  }
-
-  findFiles (parent, directory, name) {
-    const names = [].concat(name)
-    const chunks = path.resolve(parent, directory).split(path.sep)
-
-    while (chunks.length) {
-      const currentDir = chunks.join(path.sep)
-      for (const fileName of names) {
-        const filePath = path.join(currentDir, fileName)
-        if (fs.existsSync(filePath)) {
-          return filePath
-        }
-      }
-      if (parent === currentDir) {
-        break
-      }
-      chunks.pop()
-    }
-
-    return null
-  }
-
-  format (document) {
-    if (this.debug) {
-      console.time('phpcbf')
-    }
-    const text = document.getText()
-
-    let phpcbfError = false
-    const fileName =
-      TmpDir +
-      '/temp-' +
-      Math.random()
-        .toString(36)
-        .replace(/[^a-z]+/g, '')
-        .substr(0, 10) +
-      '.php'
-    fs.writeFileSync(fileName, text)
-
-    const exec = cp.spawn(this.executablePath, this.getArgs(document, fileName))
-    if (!this.debug) {
-      exec.stdin.end()
-    }
-
-    const promise = new Promise((resolve, reject) => {
-      exec.on('error', err => {
-        console.log(err)
-        reject(err)
-        if (err.code === 'ENOENT') {
-          window.showErrorMessage(
-            'PHPCBF: ' + err.message + '. executablePath not found.'
-          )
-        }
-      })
-      exec.on('exit', code => {
-        /*  phpcbf exit codes:
-                Exit code 0 is used to indicate that no fixable errors were found, so nothing was fixed
-                Exit code 1 is used to indicate that all fixable errors were fixed correctly
-                Exit code 2 is used to indicate that PHPCBF failed to fix some of the fixable errors it found
-                Exit code 3 is used for general script execution errors
-                */
-        switch (code) {
-          case 0:
-          case 1:
-          case 2:
-            const fixed = fs.readFileSync(fileName, 'utf-8')
-            if (fixed.length > 0) {
-              resolve(fixed)
-            } else {
-              reject()
-            }
-            break
-          case 3:
-            phpcbfError = true
-            break
-          default:
-            const msgs = {
-              3: 'PHPCBF: general script execution errors.',
-              16: 'PHPCBF: Configuration error of the application.',
-              32: 'PHPCBF: Configuration error of a Fixer.',
-              64: 'PHPCBF: Exception raised within the application.'
-            }
-            window.showErrorMessage(msgs[code])
-            reject()
-            break
-        }
-
-        fs.unlink(fileName, function (err) {})
-      })
-    })
-
-    if (phpcbfError) {
-      exec.stdout.on('data', buffer => {
-        console.log(buffer.toString())
-        window.showErrorMessage(buffer.toString())
-      })
-    }
-    if (this.debug) {
-      exec.stdout.on('data', buffer => {
-        console.log(buffer.toString())
-      })
-    }
-    exec.stderr.on('data', buffer => {
-      console.log(buffer.toString())
-    })
-    exec.on('close', () => {
-      // console.log(code);
-      if (this.debug) {
-        console.timeEnd('phpcbf')
-        console.groupEnd()
-      }
-    })
-
-    return promise
-  }
-
-  addRootPath (prefix) {
-    const resources = []
-    if (workspace.workspaceFolders) {
-      for (const wsFolder of workspace.workspaceFolders) {
-        resources.push(wsFolder.uri)
-      }
-    } else {
-      const editor = window.activeTextEditor
-      if (editor) {
-        resources.push(editor.document.uri)
-      }
-    }
-    for (const resource of resources) {
-      if (resource.scheme === 'file') {
-        const folder = workspace.getWorkspaceFolder(resource)
-        if (folder) {
-          const rootPath = folder.uri.fsPath
-          const tmpExecutablePath = this.executablePath.replace(
-            prefix,
-            rootPath
-          )
-          fs.exists(tmpExecutablePath, exists => {
-            if (exists) {
-              this.executablePath = tmpExecutablePath
-            }
-          })
-        }
-      }
-    }
+function onWillSaveTextDocument (event) {
+  if (isPhp(event) && !isFormatOnSaveEnabled() && phpcbf.onsave) {
+    event.waitUntil(commands.executeCommand('editor.action.formatDocument'))
   }
 }
 
+function registerTextEditorCommand (event) {
+  if (isPhp(event)) {
+    commands.executeCommand('editor.action.formatDocument')
+  }
+}
+
+function onDidChangeConfiguration () {
+  phpcbf.loadSettings()
+}
+
+// EXPORT
 exports.activate = context => {
-  const phpcbf = new PHPCBF()
-
   context.subscriptions.push(
-    workspace.onWillSaveTextDocument(event => {
-      const editor = window.activeTextEditor
-      if (
-        event.document.languageId === 'php' &&
-        phpcbf.onsave &&
-        workspace
-          .getConfiguration('editor', editor.document.uri)
-          .get('formatOnSave') === false
-      ) {
-        event.waitUntil(commands.executeCommand('editor.action.formatDocument'))
-      }
-    })
+    workspace.onWillSaveTextDocument(onWillSaveTextDocument)
   )
 
   context.subscriptions.push(
-    commands.registerTextEditorCommand('phpcbf-soderlind', textEditor => {
-      if (textEditor.document.languageId === 'php') {
-        commands.executeCommand('editor.action.formatDocument')
-      }
-    })
+    commands.registerTextEditorCommand(
+      VSCODE_COMMAND,
+      registerTextEditorCommand
+    )
   )
 
   context.subscriptions.push(
-    workspace.onDidChangeConfiguration(() => {
-      phpcbf.loadSettings()
-    })
+    workspace.onDidChangeConfiguration(onDidChangeConfiguration)
   )
 
   if (phpcbf.documentFormattingProvider) {
     context.subscriptions.push(
       languages.registerDocumentFormattingEditProvider('php', {
-        provideDocumentFormattingEdits: (document, options, token) => {
-          return new Promise((resolve, reject) => {
-            const originalText = document.getText()
-            const lastLine = document.lineAt(document.lineCount - 1)
-            const range = new Range(new Position(0, 0), lastLine.range.end)
-            phpcbf
-              .format(document)
-              .then(text => {
-                if (text !== originalText) {
-                  resolve([new vscode.TextEdit(range, text)])
-                } else {
-                  reject()
-                }
-              })
-              .catch(err => {
-                console.log(err)
-                reject(err)
-              })
-          })
+        provideDocumentFormattingEdits: async document => {
+          const originalText = document.getText()
+          const lastLine = document.lineAt(document.lineCount - 1)
+          const range = new Range(new Position(0, 0), lastLine.range.end)
+
+          let output
+          try {
+            output = phpcbf.format(document)
+          } catch (err) {
+            console.error(err.message)
+          }
+
+          if (output !== originalText) {
+            return [new vscode.TextEdit(range, output)]
+          }
         }
       })
     )
